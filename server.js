9a266c44cf4104d405d5bf336832f81686d8377f
beauspot <beauspot@outlook.com>
2023-04-27 22:18:02 +0100

    modified the server.js file by setting up the basics for the server.

--- server.js
+++ server.js
@@ -10,4 +10,60 @@ import MongodbSession from "connect-mongodb-session";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import dotenv from "dotenv";
-import logger from "morgan";
+
+dotenv.config();
+import connectDB from "./config/dbConfig.js";
+
+const app = express();
+const MongoDBStore = MongodbSession(session);
+const storemD = new MongoDBStore({
+  uri: process.env.MONGO_URL,
+  collection: "Passport-Sessions-Collection",
+  ttl: 60 * 60, // session will expire in 1hr
+});
+// Middleware functions
+app.use(
+  session({
+    resave: false,
+    secret: process.env.SESSION_SECRET_KEY,
+    saveUninitialized: false,
+    store: storemD,
+    cookie: {
+      sameSite: "strict",
+      secure: false, // use true if using https
+      maxAge: 1000 * 60 * 60, // cookie would expire in 1 hour
+    },
+  })
+);
+app.use(xss());
+app.use(cors());
+app.use(helmet());
+//app.use(logger("dev"));
+app.use(express.json());
+app.use(mongoSanitize());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+app.get("/", (req, res, next) => {
+  res.send("Hello World");
+});
+
+const Port = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(Port, () =>
+      console.log(`Server Listening on localhost:${Port}`)
+    );
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+startServer();
